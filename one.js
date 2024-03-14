4d66a97002638158c3b9ee7788348cc109fcb62c
Narendra <160315388+Narendra8731@users.noreply.github.com>
2024-03-14 20:26:10 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,21 @@
+var brand="peter England";
+var Product_Name="mens Check Shirts";
+var price=399;
+var rating=4.0;
+var discount=true;
+var avail="Y"
+var loc;
+
+console.log(brand)
+console.log(typeof brand)
+console.log(price)
+console.log(typeof price)
+console.log(typeof product_Name)
+console.log(rating)
+console.log(typeof rating)
+
+//boolean data type
+console.log(typeof discount)
+
+//undefined data type
+console.log(typeof loc)
